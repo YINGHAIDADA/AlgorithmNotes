7c4cab7d22d3401527229f2ba0db97ef661a1568
yinghaidada <pyj2897022134@gmail.com>
2024-03-04 10:50:31 +0800

    更改791，添加232

---
+++ LeetCode/232用栈实现队列.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <deque>
+
+
+using namespace std;
+
+class MyQueue {
+private:
+deque<int> dq;
+public:
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+        dq.push_back(x);
+    }
+    
+    int pop() {
+        int res = dq.front();
+        dq.pop_front();
+        return res;
+    }
+    
+    int peek() {
+        return dq.front();
+    }
+    
+    bool empty() {
+        return dq.empty();
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
+
+int main()
+{
+    MyQueue* obj = new MyQueue();
+    obj->push(1);
+    obj->push(2);
+
+    int param_2 = obj->pop();
+    int param_3 = obj->peek();
+    bool param_4 = obj->empty();
+    return 0;
+}
